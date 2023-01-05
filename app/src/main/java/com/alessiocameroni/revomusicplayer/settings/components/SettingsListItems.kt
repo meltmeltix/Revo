ab22cc8e59ee26ec63fa8404476195d623cd6f6e
alessiocameroni <alecame2002@gmail.com>
2023-01-05 19:42:11 +0100

    Added Pixely library for testing purposes

--- app/src/main/java/com/alessiocameroni/revomusicplayer/settings/components/SettingsListItems.kt
+++ app/src/main/java/com/alessiocameroni/revomusicplayer/settings/components/SettingsListItems.kt
@@ -1,103 +1,14 @@
 package com.alessiocameroni.revomusicplayer.settings.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.layoutId
-
-@Composable
-fun SettingsItem(
-    modifier: Modifier,
-    stringMainTitle: String,
-    stringSubtitle: String,
-    leadingUnit: @Composable (() -> Unit?),
-    trailingUnit: @Composable (() -> Unit?)
-) {
-    Row(modifier = modifier) {
-        val constraints = ConstraintSet {
-            val leadingBox = createRefFor("LeadingBox")
-            val trailingBox = createRefFor("TrailingBox")
-            val textTitle = createRefFor("TextTitle")
-            val textSubtitle = createRefFor("TextSubtitle")
-
-            constrain(leadingBox) {
-                start.linkTo(parent.start)
-                top.linkTo(parent.top)
-            }
-
-            constrain(textTitle) {
-                start.linkTo(leadingBox.end)
-                top.linkTo(parent.top)
-            }
-
-            constrain(textSubtitle) {
-                start.linkTo(leadingBox.end)
-                top.linkTo(textTitle.bottom)
-            }
-
-            constrain(trailingBox) {
-                top.linkTo(parent.top)
-                end.linkTo(parent.end)
-            }
-        }
-
-        ConstraintLayout(
-            constraints,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Box(
-                modifier = Modifier
-                    .layoutId("LeadingBox")
-                    .padding(horizontal = 5.dp, vertical = 8.dp)
-                    .size(60.dp)
-                    .background(Color.Red),
-                contentAlignment = Alignment.Center
-            ) {
-                leadingUnit()
-            }
-
-            Text(
-                modifier = Modifier
-                    .layoutId("TextTitle")
-                    .padding(top = 15.dp)
-                    .width(250.dp),
-                text = stringMainTitle,
-                style = MaterialTheme.typography.titleLarge,
-                fontSize = 20.sp
-            )
-
-            Text(
-                modifier = Modifier
-                    .layoutId("TextSubtitle")
-                    .width(250.dp),
-                text = stringSubtitle,
-                style = MaterialTheme.typography.bodyMedium,
-                fontSize = 14.sp
-            )
-
-            Box(
-                modifier = Modifier
-                    .layoutId("TrailingBox")
-                    .padding(horizontal = 5.dp, vertical = 8.dp)
-                    .size(60.dp)
-                    .background(Color.Red),
-                contentAlignment = Alignment.Center
-            ) {
-                trailingUnit()
-            }
-        }
-    }
-}
 
 @Composable
 fun SettingsLUnitItem(
