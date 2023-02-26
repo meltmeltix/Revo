10ad4e7001cbefac2e4bdb51182672546fcbf5c5
alessiocameroni <alecame2002@gmail.com>
2023-02-26 10:17:13 +0100

    Changed Switch logic and moved it to ListItem selectable

--- app/src/main/java/com/alessiocameroni/revomusicplayer/ui/screens/settings/library/LibrarySettingsComponents.kt
+++ app/src/main/java/com/alessiocameroni/revomusicplayer/ui/screens/settings/library/LibrarySettingsComponents.kt
@@ -1,7 +1,9 @@
 package com.alessiocameroni.revomusicplayer.ui.screens.settings.library
 
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
@@ -43,19 +45,19 @@ fun SpotifyVisibilitySelection(
     viewModel: LibrarySettingsViewModel
 ) {
     PixelyListItem(
-        headlineTextString = stringResource(id = R.string.str_showSpotifyIntegration),
-        supportingTextString = stringResource(id = R.string.desc_showSpotifyIntegration),
-        trailingContent = {
-            Switch(
-                checked = checked,
-                onCheckedChange = {
+        modifier = Modifier
+            .selectable(
+                selected = checked,
+                onClick = {
                     onChecked(!checked)
                     viewModel.saveSpotifyVisibility(
                         !checked
                     )
                 }
-            )
-        },
+            ),
+        headlineTextString = stringResource(id = R.string.str_showSpotifyIntegration),
+        supportingTextString = stringResource(id = R.string.desc_showSpotifyIntegration),
+        trailingContent = { Switch(checked = checked, onCheckedChange = null) },
         trailingContentDivider = true
     )
 }
