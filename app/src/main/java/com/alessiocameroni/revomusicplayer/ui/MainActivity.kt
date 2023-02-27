96ca23f93d6dca4e1ed27a43fd7eba465fccc74d
alessiocameroni <alecame2002@gmail.com>
2023-02-27 23:03:07 +0100

    Added SplashScreen

--- app/src/main/java/com/alessiocameroni/revomusicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/alessiocameroni/revomusicplayer/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.alessiocameroni.revomusicplayer.util.permissions.SetContentByPermission
 import com.alessiocameroni.revomusicplayer.ui.theme.RevoMusicPlayerTheme
@@ -26,6 +27,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        installSplashScreen()
 
         setContent {
             RevoMusicPlayerTheme {
