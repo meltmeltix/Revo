34eed41c56c8b376010bf870617d9aa5b7c9e741
alessiocameroni <alecame2002@gmail.com>
2023-03-25 09:57:27 +0100

    Song sorting now fully works, now to add states

--- app/src/main/java/com/alessiocameroni/revomusicplayer/ui/screens/library/songScreen/SongViewModel.kt
+++ app/src/main/java/com/alessiocameroni/revomusicplayer/ui/screens/library/songScreen/SongViewModel.kt
@@ -40,18 +40,17 @@ class SongViewModel @Inject constructor(
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), SortingOrder.ASCENDING)
 
     private var _songs: MutableStateFlow<List<Song>> = MutableStateFlow(emptyList())
-    val songs: StateFlow<List<Song>> = _songs
+    val songs: StateFlow<List<Song>> = sortingOrder
+        .combine(sortingType) { order, type -> sortList(_songs.value, order, type) }
+        .flowOn(Dispatchers.Default)
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     init {
         viewModelScope.launch {
             var list: List<Song>
             withContext(Dispatchers.IO) { list = songsRepository.getSongList() }
-            if (list.isNotEmpty()) {
-                withContext(Dispatchers.Default) {
-                    list = sortList(list, sortingOrder.value, sortingType.value)
-                }
-                _contentState.value = ContentState.SUCCESS
-            } else { _contentState.value = ContentState.FAILURE }
+            if (list.isNotEmpty()) { _contentState.value = ContentState.SUCCESS }
+            else { _contentState.value = ContentState.FAILURE }
             _songs.value = list
         }
     }
@@ -61,30 +60,28 @@ class SongViewModel @Inject constructor(
         order: SortingOrder,
         type: SortingType,
     ): List<Song> {
-        var sortedList = list
-        sortedList = when(order) {
+        return when(order) {
             SortingOrder.ASCENDING -> {
                 when(type) {
-                    SortingType.TITLE -> sortedList.sortedBy { it.songTitle }
-                    SortingType.ARTIST -> sortedList.sortedBy { it.artist }
-                    SortingType.ALBUM -> sortedList.sortedBy { it.album }
-                    SortingType.DURATION -> sortedList.sortedBy { it.duration }
-                    SortingType.DATE_ADDED -> sortedList.sortedBy { it.dateAdded }
-                    else -> { sortedList.sortedBy { it.songTitle } }
+                    SortingType.TITLE -> list.sortedBy { it.songTitle }
+                    SortingType.ARTIST -> list.sortedBy { it.artist }
+                    SortingType.ALBUM -> list.sortedBy { it.album }
+                    SortingType.DURATION -> list.sortedBy { it.duration }
+                    SortingType.DATE_ADDED -> list.sortedBy { it.dateAdded }
+                    else -> { list.sortedBy { it.songTitle } }
                 }
             }
             SortingOrder.DESCENDING -> {
                 when(type) {
-                    SortingType.TITLE -> sortedList.sortedByDescending { it.songTitle }
-                    SortingType.ARTIST -> sortedList.sortedByDescending { it.artist }
-                    SortingType.ALBUM -> sortedList.sortedByDescending { it.album }
-                    SortingType.DURATION -> sortedList.sortedByDescending { it.duration }
-                    SortingType.DATE_ADDED -> sortedList.sortedByDescending { it.dateAdded }
-                    else -> { sortedList.sortedByDescending { it.songTitle } }
+                    SortingType.TITLE -> list.sortedByDescending { it.songTitle }
+                    SortingType.ARTIST -> list.sortedByDescending { it.artist }
+                    SortingType.ALBUM -> list.sortedByDescending { it.album }
+                    SortingType.DURATION -> list.sortedByDescending { it.duration }
+                    SortingType.DATE_ADDED -> list.sortedByDescending { it.dateAdded }
+                    else -> { list.sortedByDescending { it.songTitle } }
                 }
             }
         }
-        return sortedList
     }
 
     fun setSortType(type: SortingType) {
