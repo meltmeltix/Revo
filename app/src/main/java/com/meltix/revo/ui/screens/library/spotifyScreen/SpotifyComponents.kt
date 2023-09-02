f9ce5742b9c45fc19487d1893d8cbe81cef1aa79
alessiocameroni <alecame2002@gmail.com>
2023-09-02 21:46:14 +0200

    Updated SpotifyScreen - Updated screen adapting the whole philosophy applied to the whole app so far - Added basic onRefresh method, soon to be added to each screen=

--- app/src/main/java/com/meltix/revo/ui/screens/library/spotifyScreen/SpotifyComponents.kt
+++ app/src/main/java/com/meltix/revo/ui/screens/library/spotifyScreen/SpotifyComponents.kt
@@ -1,81 +1,288 @@
 package com.meltix.revo.ui.screens.library.spotifyScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.meltix.pixely_components.RoundedDropDownMenu
 import com.meltix.revo.R
-import com.meltix.revo.ui.components.topAppBarColorOnWindowSize
-import com.meltix.revo.ui.components.topAppBarInsetsOnWindowsSize
 import com.meltix.revo.ui.navigation.RootScreens
 
-// Scaffold components
+@Composable
+fun SpotifyLayout(
+    windowClass: WindowSizeClass,
+    //viewModel: SongViewModel,
+    onOpenSpotifyClick: () -> Unit,
+    onRefresh: () -> Unit,
+    onNavigate: (String) -> Unit,
+    //contentState: ContentState,
+    //loadingContent: @Composable () -> Unit,
+    //noContent: @Composable () -> Unit,
+    content: LazyListScope.() -> Unit
+) {
+    when(windowClass.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> CompactLayout(
+            //viewModel = viewModel,
+            onOpenSpotifyClick = { onOpenSpotifyClick() },
+            onRefresh = { onRefresh() },
+            onNavigate = { onNavigate(it) },
+            //contentState = contentState,
+            //loadingContent = loadingContent,
+            //noContent = noContent,
+            content = content
+        )
+        else -> ExpandedLayout(
+            //viewModel = viewModel,
+            onOpenSpotifyClick = { onOpenSpotifyClick() },
+            onRefresh = { onRefresh() },
+            onNavigate = { onNavigate(it) },
+            //contentState = contentState,
+            //loadingContent = loadingContent,
+            //noContent = noContent,
+            content = content
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SpotifyTopActionBar(
-    navController: NavController,
-    scrollBehavior: TopAppBarScrollBehavior,
-    windowClass: WindowSizeClass
+private fun CompactLayout(
+    //viewModel: SongViewModel,
+    onOpenSpotifyClick: () -> Unit,
+    onRefresh: () -> Unit,
+    onNavigate: (String) -> Unit,
+    //contentState: ContentState,
+    //loadingContent: @Composable () -> Unit,
+    //noContent: @Composable () -> Unit,
+    content: LazyListScope.() -> Unit
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
     val expandedMenu = remember { mutableStateOf(false) }
-
-    TopAppBar(
-        title = { Text(text = stringResource(id = R.string.str_spotify)) },
-        actions = {
-            IconButton(
-                onClick = { navController.navigate(RootScreens.Search.route) }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_baseline_search_24),
-                    contentDescription = stringResource(id = R.string.str_search)
+    //val expandedSortMenu = remember { mutableStateOf(false) }
+    
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.surface
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = stringResource(id = R.string.str_spotify)) },
+                    actions = {
+                        IconButton(onClick = { /*TODO add searchbar when it is actually possible to use it*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                                contentDescription = stringResource(id = R.string.str_search)
+                            )
+                        }
+                        
+                        Box(modifier = Modifier.wrapContentSize(Alignment.TopStart)) {
+                            IconButton(onClick = { expandedMenu.value = true }) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_baseline_more_vert_24),
+                                    contentDescription = stringResource(id = R.string.str_settings)
+                                )
+                            }
+    
+                            TopBarDropDownMenu(
+                                expanded = expandedMenu,
+                                //expandedSortMenu = expandedSortMenu,
+                                onOpenSpotifyClick = { onOpenSpotifyClick() },
+                                onRefresh = { onRefresh() },
+                                onNavigate = { onNavigate(it) },
+                            )
+                            
+                            //SortDropDownMenu(expandedSortMenu, viewModel)
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
                 )
             }
-
-            Box(modifier = Modifier.wrapContentSize(Alignment.TopStart)) {
-                IconButton(onClick = { expandedMenu.value = true }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_baseline_more_vert_24),
-                        contentDescription = stringResource(id = R.string.str_settings)
-                    )
+        ) { padding ->
+            /*when(contentState) {
+                ContentState.LOADING -> loadingContent()
+                ContentState.FAILURE -> noContent()
+                ContentState.SUCCESS -> {
+                
                 }
+            }*/
+    
+            LazyColumn(
+                modifier = Modifier
+                    .padding(
+                        start = padding.calculateStartPadding(LayoutDirection.Ltr),
+                        top = padding.calculateTopPadding(),
+                        end = padding.calculateEndPadding(LayoutDirection.Rtl),
+                        bottom = 0.dp,
+                    )
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(bottom = systemBarsPadding.calculateBottomPadding()),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) { content() }
+        }
+    }
+}
 
-                TopBarDropDownMenu(
-                    expanded = expandedMenu,
-                    navController = navController
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ExpandedLayout(
+    //viewModel: SongViewModel,
+    onOpenSpotifyClick: () -> Unit,
+    onRefresh: () -> Unit,
+    onNavigate: (String) -> Unit,
+    //contentState: ContentState,
+    //loadingContent: @Composable () -> Unit,
+    //noContent: @Composable () -> Unit,
+    content: LazyListScope.() -> Unit
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
+    val expandedMenu = remember { mutableStateOf(false) }
+    //val expandedSortMenu = remember { mutableStateOf(false) }
+    
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.inverseOnSurface
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = stringResource(id = R.string.str_spotify)) },
+                    actions = {
+                        IconButton(onClick = { /*TODO add searchbar when it is actually possible to use it*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                                contentDescription = stringResource(id = R.string.str_search)
+                            )
+                        }
+                        
+                        Box(modifier = Modifier.wrapContentSize(Alignment.TopStart)) {
+                            IconButton(onClick = { expandedMenu.value = true }) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_baseline_more_vert_24),
+                                    contentDescription = stringResource(id = R.string.str_settings)
+                                )
+                            }
+    
+                            TopBarDropDownMenu(
+                                expanded = expandedMenu,
+                                //expandedSortMenu = expandedSortMenu,
+                                onOpenSpotifyClick = { onOpenSpotifyClick() },
+                                onRefresh = { onRefresh() },
+                                onNavigate = { onNavigate(it) },
+                            )
+                            
+                            //SortDropDownMenu(expandedSortMenu, viewModel)
+                        }
+                    },
+                    windowInsets = WindowInsets(top = 0.dp),
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                        scrolledContainerColor = MaterialTheme.colorScheme.inverseOnSurface
+                    ),
+                    scrollBehavior = scrollBehavior
                 )
-            }
-        },
-        windowInsets = topAppBarInsetsOnWindowsSize(windowClass),
-        colors = topAppBarColorOnWindowSize(windowClass),
-        scrollBehavior = scrollBehavior,
-    )
+            },
+            containerColor = MaterialTheme.colorScheme.inverseOnSurface
+        ) { padding ->
+            /*when (contentState) {
+                ContentState.LOADING -> loadingContent()
+                ContentState.FAILURE -> noContent()
+                ContentState.SUCCESS -> {
+                
+                }
+            }*/
+    
+            LazyColumn(
+                modifier = Modifier
+                    .padding(
+                        start = padding.calculateStartPadding(LayoutDirection.Ltr),
+                        top = padding.calculateTopPadding(),
+                        end = padding.calculateEndPadding(LayoutDirection.Rtl),
+                        bottom = 0.dp,
+                    )
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surface),
+                contentPadding = PaddingValues(bottom = systemBarsPadding.calculateBottomPadding()),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) { content() }
+        }
+    }
 }
 
 @Composable
 private fun TopBarDropDownMenu(
     expanded: MutableState<Boolean>,
-    navController: NavController
+    onOpenSpotifyClick: () -> Unit,
+    onRefresh: () -> Unit,
+    onNavigate: (String) -> Unit
 ) {
     RoundedDropDownMenu(
         expanded = expanded.value,
         onDismissRequest = { expanded.value = false }
     ) {
         DropdownMenuItem(
+            text = { Text(text = stringResource(id = R.string.str_openSpotify)) },
+            onClick = { onOpenSpotifyClick() },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_launch_spotify_24px),
+                    contentDescription = stringResource(id = R.string.str_openSpotify)
+                )
+            }
+        )
+        
+        HorizontalDivider()
+    
+        DropdownMenuItem(
+            text = { Text(text = stringResource(id = R.string.str_refresh)) },
+            onClick = { onRefresh() },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_baseline_refresh_24),
+                    contentDescription = stringResource(id = R.string.str_refresh)
+                )
+            }
+        )
+    
+        HorizontalDivider()
+        
+        DropdownMenuItem(
             text = {
                 Text(text = stringResource(id = R.string.str_settings))
             },
             onClick = {
-                navController.navigate(RootScreens.SettingsGraph.route)
+                onNavigate(RootScreens.SettingsGraph.route)
                 expanded.value = false
             },
             leadingIcon = {
--- app/src/main/java/com/meltix/revo/ui/screens/library/spotifyScreen/SpotifyScreen.kt
+++ app/src/main/java/com/meltix/revo/ui/screens/library/spotifyScreen/SpotifyScreen.kt
@@ -1,41 +1,25 @@
 package com.meltix.revo.ui.screens.library.spotifyScreen
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.meltix.revo.ui.components.contentModifier
-import com.meltix.revo.ui.components.scrollBehaviorOnWindowSize
-import com.meltix.revo.ui.components.surfaceColorOnWindowSize
 import com.meltix.revo.util.functions.findActivity
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class)
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun SpotifyFavoritesScreen(rootNavController: NavController) {
     val context = LocalContext.current
     val activity = context.findActivity()
     val windowClass = calculateWindowSizeClass(activity)
-    val scrollBehavior = scrollBehaviorOnWindowSize(windowClass)
-
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { SpotifyTopActionBar(rootNavController, scrollBehavior, windowClass) },
-        containerColor = surfaceColorOnWindowSize(windowClass),
-        content = { padding ->
-            LazyColumn(
-                modifier = Modifier.contentModifier(windowClass, padding),
-                verticalArrangement = Arrangement.spacedBy(2.dp),
-                contentPadding = PaddingValues(bottom = 70.dp)
-            ){
-
-            }
-        }
-    )
+    
+    SpotifyLayout(
+        windowClass = windowClass,
+        onOpenSpotifyClick = {
+        
+        },
+        onRefresh = { /*TODO*/ },
+        onNavigate = { rootNavController.navigate(it) }
+    ) {  }
 }
