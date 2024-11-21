d9fa5b981a977cdc9ccedb72d41d271a4fce6a05
Alessio Cameroni <alessioc.developer@gmail.com>
2024-11-21 10:44:15 +0100

    Why did you not commit with everything else

--- app/src/main/java/com/meltix/revo/ui/screens/settings/MainSettingsScreen.kt
+++ app/src/main/java/com/meltix/revo/ui/screens/settings/MainSettingsScreen.kt
@@ -1,12 +1,9 @@
 package com.meltix.revo.ui.screens.settings
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.*
 import androidx.navigation.NavController
 import com.meltix.revo.ui.theme.RevoTheme
 import com.meltix.revo.util.functions.findActivity
